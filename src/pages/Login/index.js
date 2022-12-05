6ac2560ac71f2341d49d37ebfbce45ac63566bbe
Eduardo Augusto Gomes de Oliveira <eduaugusto10@gmail.com>
2022-12-04 23:28:58 -0300

    change useState to useRef  form

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -5,20 +5,21 @@ import { login } from "../../services/auth";
 import { ToastContainer } from 'react-toastify'
 import { toastError } from "../../components/Toast";
 import { Input, Container, Title, Button, Text } from "../../styleglobal";
+import { useRef } from "react";
 
 export default function Login() {
   const { signIn } = useContext(AuthContext);
-
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const passRef = useRef()
+  const emailRef = useRef()
 
   async function handleSubmit(e) {
     e.preventDefault();
+
     try {
       api.post("/login",
         {
-          email,
-          password
+          email: emailRef.current.value,
+          password: passRef.current.value
         }).then((result) => {
           signIn(result.data.token);
           login(result.data.token);
@@ -33,21 +34,21 @@ export default function Login() {
     <Container>
       <ToastContainer />
       <Title>Login</Title>
-      <Text>E-mail</Text>
-      <Input
-        placeholder="Insira o e-mail"
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <Text>Senha</Text>
-      <Input
-        placeholder="Insira a senha"
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <Button onClick={handleSubmit}>Entrar</Button>
+      <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} onSubmit={handleSubmit}>
+        <Text>E-mail</Text>
+        <Input
+          placeholder="Insira o e-mail"
+          type="email"
+          ref={emailRef}
+        />
+        <Text>Senha</Text>
+        <Input
+          placeholder="Insira a senha"
+          type="password"
+          ref={passRef}
+        />
+        <Button type="submit">Entrar</Button>
+      </form>
     </Container>
   );
 }
